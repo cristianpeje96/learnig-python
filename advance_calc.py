fb28b2c67b739402d2291ab4a265f414cf134933
Cristian Peje <cristianfernandopejendino@gmail.com>
2024-01-20 12:58:26 -0500

    upload avance calc

---
+++ advance_calc.py
@@ -0,0 +1,12 @@
+'''crear una calculadora que reciba dos valores 
+por consola y realice las operaciones aritmeticas basicas'''
+import os 
+os.system('clear') 
+ 
+#inputs
+print("Ingrese primer valor: ")
+n1 = int(input ())
+n2 = int(input("Ingrese el segundo valor: "))
+suma = n1 + n2
+print("suma: ", suma)
+
---
+++ aleatorios1.py
@@ -0,0 +1,12 @@
+'''crear una calculadora que reciba dos valores 
+por consola y realice las operaciones aritmeticas basicas'''
+
+import os 
+from random import randint, uniform
+
+os.system('clear')
+n1 = randint(1,100)
+n2 = uniform(1,100)
+
+print(n1)
+print(n2)
