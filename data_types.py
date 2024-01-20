9a8baffa7891685364d3ab5ca365e907cd5424df
Cristian Peje <cristianfernandopejendino@gmail.com>
2024-01-20 12:06:37 -0500

    first commit data_types

---
+++ data_types.py
@@ -0,0 +1,50 @@
+#tipos de datos en Python
+#Python es un lenguje latamente equipado
+
+#1. Numéricos
+##Enteros Z
+num1 = 42
+print("num1: ", type(num1))
+
+##Flotantes o decimales (reales)
+num2 = 50.5
+print("num2: ", type(num2))
+
+##Complejos => complex
+num3 = 2j
+print("num3: ", type(num3))
+
+print("\n")
+#2. Cadena 
+my_name = "Cristian"
+print("my_name: ", type(my_name))
+my_lastname = 'Pejendino'
+print("my_lastname: ", type(my_lastname))
+profile = '''Cristian es una personas 
+            enfocada en el equipo'''
+print("profile: ", type(profile))
+address = 'carrera 8#16-11'
+print("address: ", type(address))
+phone_number = "3125647016"
+print("phono_number: ", type(phone_number))
+
+print("\n")
+#3. Logicos (valores o expresiones booleanas)
+usuario_activo = True
+print("usuario_activo: ", type(usuario_activo))
+pago_realizado = False
+print("pago_realizado: ", type(pago_realizado))
+
+print("\n")
+#4. Listas
+frutas = ['apple', 'banana']
+print("frutas: ", type(frutas))
+print(frutas)
+colors = ['blu', 'red', 'blak']
+print("colors: ", type(colors))
+print(colors)
+
+
+#5. Duplas
+#6. Diccionarios
+#7. Conjuntos
